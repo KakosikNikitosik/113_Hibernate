ae375fd04b40bfde4ff6e6a7a1f9ff1d3bfb7f13
KakosikNikitosik <thegoodness28@gmail.com>
2023-06-05 19:02:49 +0300

    created connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,26 @@
 package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
-import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    Connection connection;
-    Driver driver = new ();
+    public static void main(String[] args) {
+        try{
+            String url = "jdbc:mysql://localhost/mytestdb";
+            String username = "root";
+            String password = "root1234";
+            //Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection connection = DriverManager.getConnection(url, username, password)){
+
+                System.out.println("Connection succesfull!");
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
+    }
 }
