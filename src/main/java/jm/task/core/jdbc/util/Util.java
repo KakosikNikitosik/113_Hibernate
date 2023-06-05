3e1aedba293ad7a365966c671ce5c27b870d9981
KakosikNikitosik <thegoodness28@gmail.com>
2023-06-05 18:54:11 +0300

    added toString

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,10 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    Connection connection;
+    Driver driver = new ();
 }
